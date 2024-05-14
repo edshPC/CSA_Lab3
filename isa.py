83e91eb60fc1937ee65fcf2ae22828cefd339b02
edshPC <ed_shh@mail.ru>
2024-05-14 19:30:26 +0300

    Ultra translator

--- isa.py
+++ isa.py
@@ -4,13 +4,38 @@ import re
 
 
 class Opcode(str, Enum):
+    WORD = "word"
+    DB = "db"
+    RESW = "resw"
+
+    NOP = "nop"
     PUSH = "push"
+    POP = "pop"
+
+    ADD = "add"
+    SUB = "sub"
+    MUL = "mul"
+    DIV = "div"
+    MOD = "mod"
+    INC = "inc"
+    DEC = "dec"
+    DUP = "dup"
+
+    # Оператор загрузки литералов в память, не может быть вызван из программы
+    _MEM = "mem"
 
     @property
     def arg_count(self):
         return {
-            self.PUSH: 1
-        }[self]
+            self.WORD: (1, 1024),
+            self.DB: (1, 1024),
+            self.RESW: (1, 1),
+
+            self.PUSH: (1, 1),
+            self.POP: (0, 1),
+
+            self._MEM: (-1, -1)
+        }.get(self, (0, 0))
 
     def __str__(self):
         return str(self.value)
--- translator.py
+++ translator.py
@@ -12,50 +12,78 @@ def get_meaningful(text):
 def translate_stage_1(text: str):
     code = []
     labels = {}
+    pc = 1
 
     for token in text.splitlines():
         token = token.strip()
-        pc = len(code)
 
         if token.endswith(":"):  # токен содержит метку
             label = token.strip(":")
             assert label not in labels, f"Redefinition of label: {label}"
             labels[label] = pc
         else:  # токен содержит инструкцию
-            mnemonic, *args = re.split(r',? +', token)
-            opcode = Opcode(mnemonic)
-            assert len(args) == opcode.arg_count, f"Invalid number of args for '{mnemonic}': Expected {opcode.arg_count}, got {len(args)}"
+            mnemonic, *args = re.findall(r'(?:"[^"]*"|[^,\s])+', token) # разделение по пробелу не в кавычках
+            opcode = Opcode(mnemonic.lower())
+            a, b = opcode.arg_count
+            assert a <= len(args) <= b, f"Invalid number of args for '{mnemonic}': Expected [{a};{b}], got {len(args)}"
+
+            pc_diff = 1
+            if opcode == Opcode.WORD or opcode == Opcode.DB: # Команда сохранения константы
+                output = []
+                for arg in args:
+                    if re.fullmatch(r'".*"', arg):
+                        # Строку сохраняем посимвольно
+                        output.extend(ord(x) for x in arg.strip('"'))
+                    else:
+                        output.append(int(arg, 0))
+                args = output
+                opcode = Opcode._MEM
+                pc_diff = len(output)
+            elif opcode == Opcode.RESW:
+                pc_diff = int(args[0], 0)
+                assert pc_diff > 0, "Cannot reserve negative space"
+                args = []
+                opcode = Opcode._MEM
+
             code.append({"index": pc, "opcode": opcode, "args": args})
+            pc += pc_diff
 
     return labels, code
 
 
 def translate_stage_2(labels, code):
-    print(labels)
+    assert "_start" in labels, "Can't find '_start' label"
+    startpos = labels["_start"]
     for instruction in code:
         args = instruction["args"]
         for i in range(len(args)):
+            if instruction["opcode"] == Opcode._MEM:
+                continue
+
             if re.fullmatch(r'[a-z_]+', args[i], flags=re.IGNORECASE):
+                # Если это метка
                 assert args[i] in labels, f"Label not defined: {args[i]}"
                 args[i] = labels[args[i]]
             else:
+                # Если это литерал
                 assert re.fullmatch(r'0x[\da-f]+|\d+', args[i], flags=re.IGNORECASE), f"Unknown type of argument: {args[i]}"
-    return code
+    return startpos, code
 
 
 def translate(text):
     text = get_meaningful(text)  # Удаление комментариев и пустых строк
     labels, code = translate_stage_1(text)
-    return translate_stage_2(labels, code)
+    startpos, code = translate_stage_2(labels, code)
+    return {"start": startpos, "code": code}
 
 
 def main(source, target):
     with open(source, encoding="utf-8") as f:
         source = f.read()
 
-    code = translate(source)
-    write_code(target, code)
-    print("source LoC:", source.count("\n")+1, "code instr:", len(code))
+    program = translate(source)
+    write_code(target, program)
+    print("source LoC:", source.count("\n")+1, "code instr:", len(program["code"]))
 
 
 if __name__ == "__main__":
